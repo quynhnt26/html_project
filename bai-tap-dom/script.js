1adbfc07484654c818709d28057515545d66ce72
quynhnt26 <83715348+quynhnt26@users.noreply.github.com>
2021-05-07 18:12:05 +0700

    Add files via upload

---
+++ bai-tap-dom/script.js
@@ -0,0 +1,18 @@
+console.log("RUN SCRIPT");
+
+// Đổi style của tiêu đề Our companies thành chữ in đậm và kích thước 32px
+// Gợi ý: Sử dụng .style.fontWeight và .style.fontSize
+document.querySelector("h2.title-section") .style.fontWeight = "bold";
+document.querySelector("h2.title-section") .style.fontSize = "32px";
+
+// Đổi màu nền của phần footer thành màu #0984e3
+// Gợi ý: Sử dụng .style.backgroundColor
+document.getElementById("OMG-footer") .style.backgroundColor = "#0984e3";
+
+// Đổi đoạn text ở footer từ "© 2020 One Mount Group, JSC." thành "© 2021 One Mount Group, JSC."
+// Gợi ý: Sử dụng .innerText
+document.querySelectorAll("p")[2] .innerText = "© 2021 One Mount Group, JSC."
+
+// Đổi ảnh đầu tiên thành ảnh onhousing.png
+// Gợi ý: Sử dụng .src
+document.querySelectorAll("img")[0] .src = "img/onhousing.png"
